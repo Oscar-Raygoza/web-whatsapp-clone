26d4369d6f749bcaa6d72d43df03d228736bbf1b
Oscar Eduardo Miramontes Raygoza <oscar.eduardo.raygoza@gmail.com>
2020-09-07 17:47:38 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB52zehSSFP_b5NP-IDVj5_bKEEACwhJuw",
+  authDomain: "web-whatsapp-clone-23z32.firebaseapp.com",
+  databaseURL: "https://web-whatsapp-clone-23z32.firebaseio.com",
+  projectId: "web-whatsapp-clone-23z32",
+  storageBucket: "web-whatsapp-clone-23z32.appspot.com",
+  messagingSenderId: "457907058785",
+  appId: "1:457907058785:web:6671e6e091eda6bfbd9220",
+};
