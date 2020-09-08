22f3bee4f3d4b6f121634450a03c0f0a6280c458
Oscar Eduardo Miramontes Raygoza <oscar.eduardo.raygoza@gmail.com>
2020-09-08 01:13:33 -0500

    update design

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -33,21 +33,98 @@ export default function () {
         </div>
       </div>
       <div className="chat__body">
+        <div className="chat__date">
+          <span className="chat__date--span">
+            {new Date().toLocaleString("en-US", {
+              weekday: "long",
+            })}
+          </span>
+        </div>
         <p className="chat__message">
+          <span data-testid="tail-in" data-icon="tail-in" class="_2-dPL">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 8 13"
+              width="8"
+              height="13"
+            >
+              <path
+                opacity=".13"
+                fill="#0000000"
+                d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"
+              ></path>
+            </svg>
+          </span>
           <span className="chat__name">Oscar</span> this is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          <span className="chat__timestamp">
+            {new Date().toLocaleString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })}
+          </span>
         </p>
+
         <p className="chat__message">
+          <span data-testid="tail-in" data-icon="tail-in" class="_2-dPL">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 8 13"
+              width="8"
+              height="13"
+            >
+              <path
+                opacity=".13"
+                fill="#0000000"
+                d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"
+              ></path>
+            </svg>
+          </span>
           <span className="chat__name">Oscar</span> this is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          <span className="chat__timestamp">
+            {new Date().toLocaleString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })}
+          </span>
         </p>
+
         <p className="chat__message chat__reciver">
+          <span data-testid="tail-in" data-icon="tail-in" class="_2-dPL">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 8 13"
+              width="8"
+              height="13"
+            >
+              <path
+                opacity=".13"
+                fill="#0000000"
+                d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"
+              ></path>
+            </svg>
+          </span>
           <span className="chat__name">Oscar</span> this is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Oscar</span> this is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+          <span className="chat__timestamp">
+            {new Date().toLocaleString("en-US", {
+              hour: "numeric",
+              minute: "numeric",
+              hour12: true,
+            })}
+          </span>
         </p>
       </div>
 
